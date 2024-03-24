d32fe8e661b15c2d0935dad35741849a0f0ba6ee
Dacosta <dacsolo10@gmail.com>
2024-03-24 07:22:16 +0000

    added a hook to load todays activities on the bookings

---
+++ src/features/dashboard/useTodaysActivity.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getStaysTodayActivity } from "../../services/apiBookings";
+
+export function useTodaysActivity() {
+  const { isPending: isLoadingTodaysActivities, data: todaysActivities } =
+    useQuery({
+      queryKey: ["todays-activity"],
+      queryFn: getStaysTodayActivity,
+    });
+
+  return { isLoadingTodaysActivities, todaysActivities };
+}
