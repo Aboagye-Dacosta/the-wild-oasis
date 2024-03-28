89d13a82755663901c45a3c9dc49f526aba2004a
Dacosta <dacsolo10@gmail.com>
2024-03-28 17:50:25 +0000

    added links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Heading from "../ui/Heading";
 import Logo from "../ui/Logo";
+import Row from "../ui/Row";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -19,6 +20,32 @@ function Login() {
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
+      <Row type="horizontal"  id="badges">
+        <a href="https://www.linkedin.com/in/solomon-aboagye-011776210/">
+          <img
+            src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white"
+            alt="LinkedIn Badge"
+          />
+        </a>
+        <a href="https://dacostasolomon-codeman.hashnode.dev">
+          <img
+            src="https://img.shields.io/badge/Hashnode-blue?style=for-the-badge&logo=hashnode&logoColor=white"
+            alt="Hashnode Badge"
+          />
+        </a>
+        <a href="https://twitter.com/CODE_COSTA">
+          <img
+            src="https://img.shields.io/badge/Twitter-blue?style=for-the-badge&logo=twitter&logoColor=white"
+            alt="Twitter Badge"
+          />
+        </a>
+        <a href="https://github.com/Aboagye-Dacosta">
+          <img
+            src="https://img.shields.io/badge/Github-black?style=for-the-badge&logo=github&logoColor=white"
+            alt="Twitter Badge"
+          />
+        </a>
+      </Row>
     </LoginLayout>
   );
 }
